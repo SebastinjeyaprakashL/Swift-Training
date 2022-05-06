39d5ae20eaf43a8a7ab65f445c6fda2404d01c23
SebastinjeyaprakashL <79531993+SebastinjeyaprakashL@users.noreply.github.com>
2022-05-06 15:18:03 +0530

    Update protocolDemo.swift

--- protocolDemo.swift
+++ protocolDemo.swift
@@ -1,22 +1,28 @@
 import Foundation
 import Glibc
- 
+
 protocol ThirdPartyProto {
     var name : String {get}
     var license : String {get}
 }
 
+protocol LibraryProto {
+    func printAllLicenses (_ thirdParties :[ThirdPartyProto])
+}
+
 class ThirdParty : ThirdPartyProto {
     var name : String
     var license : String 
-    
+
     init (name : String, license : String){
         self.name = name
         self.license = license
-    }    
+    }
+    
+    
 }
 
-class Library {
+class Library : LibraryProto{
     func printAllLicenses (_ thirdParties : [ThirdPartyProto]) {
         for thirdParty in thirdParties {
             print ("Name : \(thirdParty.name)  \nLicense : \(thirdParty.license)")
@@ -24,19 +30,23 @@ class Library {
     }
 }
 
+func addNewThirdParty (_ newParty : ThirdPartyProto){
+    thirdPartyArray.append(newParty)
+}
+
+var thirdPartyArray = [ThirdPartyProto]()
+
 let lottieLicense = "License description of Lottie"
 let lottie = ThirdParty (name : "Lottie", license : lottieLicense )
+addNewThirdParty(lottie)
 
 let progressHUDSDKlicense = "License description of progress HUDSDK"
 let progressHUDSDK = ThirdParty (name : "Progress HUDSDK", license : progressHUDSDKlicense )
+addNewThirdParty(progressHUDSDK)
 
 let newSDKLicense = "License description of New SDK"
 let newSDK = ThirdParty (name : "New SDK", license : newSDKLicense)
-
-var thirdPartyArray = [ThirdPartyProto]()
-thirdPartyArray.append(lottie)
-thirdPartyArray.append(progressHUDSDK)
-thirdPartyArray.append(newSDK)
+addNewThirdParty(newSDK)
 
 if !thirdPartyArray.isEmpty{
     Library().printAllLicenses(thirdPartyArray)
